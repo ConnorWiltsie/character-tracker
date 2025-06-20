066c410e90c52714182a5e3d2796fc01ec925f68
wilts <wiltsie.connor@gmail.com>
2025-06-20 09:46:48 -0400

    Added Character and Weapon

---
+++ src/main/java/com/connorwiltsie/fallout/charactertracker/entity/Character.java
@@ -0,0 +1,75 @@
+package com.connorwiltsie.fallout.charactertracker.entity;
+import java.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "character")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class Character {
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    //General character information
+    private String name;
+    private String race;
+    private String background;
+    private int level;
+    private int xp;
+
+    // SPECIAL attributes
+    private int strength;
+    private int perception;
+    private int endurance;
+    private int charisma;
+    private int intelligence;
+    private int agility;
+    private int luck;
+
+    // Health and combat stats
+    private int armorClass;
+    private int damageThreshold;
+    private int staminaPoints;
+    private int hitPoints;
+    private int healingRate;
+    private int actionPoints;
+
+    // Derived/conditional states
+    private String currentConditions;
+    private String combatSequence;
+    private String passiveSense;
+    private String partyNerve;
+    private String groupSneak;
+
+    // Fatigue/Survival conditions
+    private int hungerLevel;
+    private int dehydrationLevel;
+    private int exhaustionLevel;
+    private int radiationLevel;
+    private int radsLevel;
+
+    // Inventory & economy
+    private int caps;
+    private int carryLoad;
+
+    // Skills
+    private int barter;
+    private int breach;
+    private int crafting;
+    private int energyWeapons;
+    private int explosives;
+    private int guns;
+    private int intimidation;
+    private int medicine;
+    private int meleeWeapons;
+    private int science;
+    private int sneak;
+    private int speech;
+    private int survival;
+    private int unarmed;
+    private int pilot;
+}

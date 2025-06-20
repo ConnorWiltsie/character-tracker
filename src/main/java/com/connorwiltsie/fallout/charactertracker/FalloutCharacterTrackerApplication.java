c69d03a872a3b3f891aac01072cd2c79e108fa13
wilts <wiltsie.connor@gmail.com>
2025-06-20 08:44:34 -0400

    Initial commit

---
+++ src/main/java/com/connorwiltsie/fallout/charactertracker/FalloutCharacterTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.connorwiltsie.fallout.charactertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalloutCharacterTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalloutCharacterTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connorwiltsie/fallout/charactertracker/FalloutCharacterTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connorwiltsie.fallout.charactertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalloutCharacterTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
